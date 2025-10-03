e7121b39c2a900f1bc4a883553cf4bbe6dd46923
PradnyaBhosale03 <pradnyabhosale0302@gmail.com>
2025-10-03 13:56:19 +0530

    Number programs

---
+++ Numbers/PowerOftwoseries.java
@@ -0,0 +1,13 @@
+class PowerOftwoseries
+{
+	public static void main(String[] args)
+	{
+		int n = 10;
+		int term = 2;
+		for(int i=1; i<=n; i++)
+		{
+			System.out.println(term);
+			term = term*2;
+		}
+	}
+}
